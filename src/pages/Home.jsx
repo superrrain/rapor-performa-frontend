b1ef4a9892a6a3b52ab1ba559d97ed7b82c32e33
Superrrain <rrainugraha@gmail.com>
2025-06-02 18:16:47 +0700

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="p-4 text-center text-xl">
+      <h1 className="text-2xl font-bold mb-4">Rapor Performa Toko</h1>
+      <p>Selamat datang di dashboard performa toko.</p>
+    </div>
+  )
+}
